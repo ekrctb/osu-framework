2e64f185648b8655b52f4b3cd0c6edd349955011
ekrctb <ekrctb@gmail.com>
2018-08-30 19:11:20 +0900

    perspective

---
+++ osu.Framework.Tests/Visual/TestCasePerspective.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-framework/master/LICENCE
+
+using osu.Framework.Extensions.MatrixExtensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
+using OpenTK;
+
+namespace osu.Framework.Tests.Visual
+{
+    public class TestCasePerspective : TestCase
+    {
+        private readonly TestBrowser browser;
+        public TestCasePerspective()
+        {
+            var perspectiveContainer = new PerspectiveContainer
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both
+            };
+            browser = new TestBrowser();
+            //perspectiveContainer.ChildrenEnumerable = Enumerable.Range(0, 100).Select(i => new Box
+            //{
+            //    Anchor = Anchor.Centre,
+            //    Position = new Vector2(RNG.NextSingle() * 2 - 1, RNG.NextSingle() * 2 - 1) * 100,
+            //    Colour = new Color4(RNG.NextSingle(), RNG.NextSingle(), RNG.NextSingle(), .5f),
+            //    Size = new Vector2(10),
+            //});
+            perspectiveContainer.Add(new Container {
+                Size = new Vector2(1000),
+                Scale = new Vector2(.5f),
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Child = browser
+            });
+
+            Add(new Container
+            {
+                Child = perspectiveContainer,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+            });
+            AddSliderStep("A", -2, 2, 0f, x =>
+            {
+                perspectiveContainer.A = x / 100;
+                perspectiveContainer.Invalidate();
+            });
+            AddSliderStep("B", -2, 2, 0f, x =>
+            {
+                perspectiveContainer.B = x / 100;
+                perspectiveContainer.Invalidate();
+            });
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            browser.LoadTest(typeof(TestCaseSizing));
+        }
+
+        public class PerspectiveContainer : Container
+        {
+            public float A, B;
+            public override DrawInfo DrawInfo
+            {
+                get
+                {
+                    DrawInfo di = Parent?.DrawInfo ?? new DrawInfo(null);
+
+                    Vector2 translation = DrawPosition + AnchorPosition + Parent.ChildOffset;
+                    Vector2 origin = OriginPosition;
+
+                    MatrixExtensions.TranslateFromLeft(ref di.Matrix, translation);
+
+                    di.Matrix = new Matrix3(1, 0, A, 0, 1, B, 0, 0, 1) * di.Matrix;
+
+                    MatrixExtensions.TranslateFromLeft(ref di.Matrix, -origin);
+
+                    di.MatrixInverse = di.Matrix.Inverted();
+                    return di;
+                }
+            }
+        }
+    }
+}
--- osu.Framework/Graphics/Vector2Extensions.cs
+++ osu.Framework/Graphics/Vector2Extensions.cs
@@ -24,8 +24,10 @@ namespace osu.Framework.Graphics
         /// <param name="result">The transformed vector</param>
         public static void Transform(ref Vector2 pos, ref Matrix3 mat, out Vector2 result)
         {
-            result.X = mat.Row0.X * pos.X + mat.Row1.X * pos.Y + mat.Row2.X;
-            result.Y = mat.Row0.Y * pos.X + mat.Row1.Y * pos.Y + mat.Row2.Y;
+            var w = mat.Row0.Z * pos.X + mat.Row1.Z * pos.Y + mat.Row2.Z;
+            var invW = w == 1 ? 1 : 1 / w;
+            result.X = (mat.Row0.X * pos.X + mat.Row1.X * pos.Y + mat.Row2.X) * invW;
+            result.Y = (mat.Row0.Y * pos.X + mat.Row1.Y * pos.Y + mat.Row2.Y) * invW;
         }
 
         /// <summary>
